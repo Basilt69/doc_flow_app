9d2fa78f9f510c31fd4a21a4b2d34dc76caab5ee
Basil Tkachenko <baziltkachenko@gmail.com>
2023-07-21 12:26:05 +0300

    fastapi lessons 5 (authentication and registration)

---
+++ lesson_5/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Depends
+from fastapi_users import fastapi_users, FastAPIUsers
+
+from lesson_5.auth.auth import auth_backend
+from lesson_5.auth.database import User
+from lesson_5.auth.manager import get_user_manager
+from lesson_5.auth.schemas import UserRead, UserCreate
+
+app = FastAPI(
+    title="Documents Flow App"
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
+@app.get("/unprotected-route")
+def unprotected_route(user: User = Depends(current_user)):
+    return f"Hello anonym"
